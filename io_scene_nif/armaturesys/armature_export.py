799466caf977240771dfb6bc5da2181295761097
CommanderC22 <30336381+CommanderC22@users.noreply.github.com>
2017-08-17 19:07:58 +1200

    Fix armature export
    armatures export fine now, just needed to import Nifops

--- io_scene_nif/armaturesys/armature_export.py
+++ io_scene_nif/armaturesys/armature_export.py
@@ -41,6 +41,7 @@ import bpy
 import mathutils
 from io_scene_nif.utility import nif_utils
 from io_scene_nif.utility.nif_logging import NifLog
+from io_scene_nif.utility.nif_global import NifOp
 
 from pyffi.formats.nif import NifFormat
 
